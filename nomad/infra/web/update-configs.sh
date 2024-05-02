2b0ef5cf4c8aaa8faa7c86d98f88b737a3c23cf7
Dave O'Connor <git@mail.andvari.net>
2024-05-02 22:16:21 +0100

    derp

--- nomad/infra/web/update-configs.sh
+++ nomad/infra/web/update-configs.sh
@@ -3,4 +3,4 @@
 # go install github.com/gerrowadat/nomad-homelab/nomad-conf@latest
 
 nomad-conf upload haproxy.cfg nomad/jobs/web:haproxy_cfg
-nomad-conf upload default.cf nomad/jobs/web:nginx_cf
+nomad-conf upload default.conf nomad/jobs/web:nginx_cf
